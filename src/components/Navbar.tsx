b420a6f4c08414d28e7f7206125e7acafd9b4da4
Mohamed Hazeem <a.mohamedhazeem@gmail.com>
2023-11-20 20:08:05 +0530

    Initial commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { IconContext } from "react-icons";
+import { FaFileSignature } from "react-icons/fa";
+
+function Navbar() {
+  return (
+    <div className="navbar-container">
+      <div className="navbar-icon-text-container">
+        <IconContext.Provider value={{ color: "black", className: "" }}>
+          <div>
+            <FaFileSignature size={50} />
+          </div>
+        </IconContext.Provider>
+        <input
+          type="text"
+          placeholder="File Name"
+          className="navbar-fileinput border-2 border-gray-400 focus:outline-orange-600"
+        />
+      </div>
+
+      <div className="navbar-buttons">
+        <button type="button" className="navbar-button save">
+          SAVE
+        </button>
+        <button type="button" className="navbar-button clear">
+          CLEAR
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
